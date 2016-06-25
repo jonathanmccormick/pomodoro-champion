eb1b82134ab2c800edc3b9966ed4958691c6535d
Jonathan McCormick <hello@jmccormick.me>
2016-06-25 17:32:41 -0600

    Implement custom Bootstrap theme

--- config/assets/default.js
+++ config/assets/default.js
@@ -5,7 +5,9 @@ module.exports = {
     lib: {
       css: [
         'public/lib/bootstrap/dist/css/bootstrap.css',
-        'public/lib/bootstrap/dist/css/bootstrap-theme.css'
+        // 'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+        'public/themes/CosmoTheme.min.css'
+
       ],
       js: [
         'public/lib/angular/angular.js',
