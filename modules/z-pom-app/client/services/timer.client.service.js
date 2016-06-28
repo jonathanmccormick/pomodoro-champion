d17380435838c38e102dc2e047bded24f8c5888d
Jonathan McCormick <hello@jmccormick.me>
2016-06-28 07:43:12 -0600

    Bug fix when pom contains seconds

--- modules/z-pom-app/client/services/timer.client.service.js
+++ modules/z-pom-app/client/services/timer.client.service.js
@@ -39,7 +39,7 @@ angular
               &&
               self.prefs.confirmPomCancel === true
               &&
-              self.minutesRemaining <= self.prefs.pomCancelConfirmationThresholdMinutes
+              self.secondsRemaining/60 <= self.prefs.pomCancelConfirmationThresholdMinutes
             ) {
           $('#cancelModal').modal('show');
         } else {
