dab436ad0b5849e964e0156e11cac4df61e9b548
Jonathan McCormick <hello@jmccormick.me>
2016-07-05 08:57:32 -0600

    Refactor Pom Schema

--- modules/z-pom-app/server/models/poms.server.model.js
+++ modules/z-pom-app/server/models/poms.server.model.js
@@ -3,64 +3,40 @@
 // Module dependencies.
 var mongoose = require('mongoose');
 var User = mongoose.model('User');
-// var moment = require('moment');
 
-// Add PomLogs data schema to user
-User.schema.add({
-  pomLogs: [
+var PomSchema = new mongoose.Schema({
+  // task: {
+  //   type: ObjectID
+  // },
+  momentStarted: {
+    type: Date,
+    required: true,
+    default: null
+  },
+  momentCompleted: {
+    type: Date,
+    required: true,
+    default: null
+  },
+  pauses: [
     {
-      date: {
-        type: String,
+      momentPaused: {
+        type: Date,
         required: true,
-        default: moment().format('YYYY-MM-DD')
+        default: null
       },
-      pomsCompleted: {
-        type: Number,
+      momentResumed: {
+        type: Date,
         required: true,
-        default: 0
-      },
-      pomsFailed: {
-        type: Number,
-        required: true,
-        default: 0
+        default: null
       }
     }
-  ]
+  ],
+  notes: {
+    type: String
+  }
 });
 
 User.schema.add({
-  poms: [
-    {
-      // task: {
-      //   type: ObjectID
-      // },
-      momentStarted: {
-        type: Date,
-        required: true,
-        default: null
-      },
-      momentCompleted: {
-        type: Date,
-        required: true,
-        default: null
-      },
-      pauses: [
-        {
-          momentPaused: {
-            type: Date,
-            required: true,
-            default: null
-          },
-          momentResumed: {
-            type: Date,
-            required: true,
-            default: null
-          }
-        }
-      ],
-      notes: {
-        type: String
-      }
-    }
-  ]
+  poms: [PomSchema]
 });
