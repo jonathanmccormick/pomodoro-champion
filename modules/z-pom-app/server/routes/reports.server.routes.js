281b32d3798bba47d1fd62429928d4df7863f04f
Jonathan McCormick <hello@jmccormick.me>
2016-08-22 16:59:36 -0600

    Change get report MongoDB query to work with new data scheme
    Now returns pom objects, not days.

--- modules/z-pom-app/server/routes/reports.server.routes.js
+++ modules/z-pom-app/server/routes/reports.server.routes.js
@@ -9,31 +9,24 @@ module.exports = function(app) {
   app.route('/api/user/reports/:startDate/:endDate')
   .get(function(req, res) {
 
-    // Create the dates array
-    var dates = []; // Create array
-    dates.push(req.params.startDate); // add startDate to the array
-    var workingDate = new Date(req.params.startDate + 'CST'); // convert startDate to JS date object
-    while (workingDate < (new Date(req.params.endDate + 'CST'))) {
-      workingDate.setDate(workingDate.getDate() + 1); // increment date object by one day
-      dates.push(moment(workingDate).format('YYYY-MM-DD')); // create a string of our format from that date object and push that new string to the array
-    }
-
     User.aggregate([
         { '$match': { '_id': req.user._id }},
-        { '$unwind': '$pomLogs' },
+        { '$unwind': '$poms' },
         { '$project': {
             '_id': 0,
-            'pomLogs.date': 1,
-            'pomLogs.pomsCompleted': 1,
-            'pomLogs.pomsFailed': 1
+            'poms.momentStarted': 1,
+            'poms.momentCompleted': 1
         }},
-        { '$match': { 'pomLogs.date': { $in: dates }}}
+        { '$match': { 'poms.momentStarted': { '$gte': new Date(req.params.startDate)} } },
+        { '$match': { 'poms.momentCompleted': { '$lte': new Date(req.params.endDate)} } }
+
       ],
       function(err,result) {
         if (err) {
           console.log(err);
           res.status(500);
         }
+        console.log(result);
         res.json(result);
       }
     );
