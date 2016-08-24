6c5ea48d918053f9b205902b4c14f6900c681398
Jonathan McCormick <hello@jmccormick.me>
2016-08-24 13:56:01 -0600

    Change pom completion route to work with new collection

--- modules/z-pom-app/server/routes/complete.pom.server.routes.js
+++ modules/z-pom-app/server/routes/complete.pom.server.routes.js
@@ -1,22 +1,26 @@
 'use strict';
 
-var mongodb = require('mongodb');
+// var mongodb = require('mongodb');
 var mongoose = require('mongoose');
-var User = mongoose.model('User');
+// var User = mongoose.model('User');
+var Pom = mongoose.model('Pom');
 
 module.exports = function(app) {
 
   // Find pom with current ID, then set momentCompleted
   app.route('/api/user/pom/:id/complete/:moment')
   .put(function(req, res) {
-    console.log(req.params.id);
-    User.findOneAndUpdate(
-      { '_id': req.user._id, 'poms._id': req.params.id },
-      { $set: { 'poms.$.momentCompleted': req.params.moment  } },
+    
+    Pom.findOneAndUpdate(
+      { '_id': req.params.id, 'userID': req.user._id },
+      { $set: { 'momentCompleted': req.params.moment  } },
       { save: true, new: true },
       function(err, doc) {
         if (err) return err;
+        console.log(doc);
+        res.sendStatus(200);
       });
+
   });
 
 };
