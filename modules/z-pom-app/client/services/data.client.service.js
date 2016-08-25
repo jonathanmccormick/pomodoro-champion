dc7d9fc8feac8fb89e8093b58336d9480b179011
Jonathan McCormick <hello@jmccormick.me>
2016-08-25 15:37:38 -0600

    Remove obsolete code from client data service

--- modules/z-pom-app/client/services/data.client.service.js
+++ modules/z-pom-app/client/services/data.client.service.js
@@ -4,41 +4,8 @@ angular.module('z-pom-app').service('dataService', [ '$http', function($http) {
 
   var self = this;
 
-  // return local date in YYYY-MM-DD
-  self.today = function() {
-    return moment().format('YYYY-MM-DD');
-  };
-
-  self.addPom = function() {
-    $http.put('/api/user/poms/' + self.today())
-      .success(function (status){
-      })
-      .error(function(status){
-        console.log('Total failure, try again.');
-      });
-  };
-
-  self.addFailedPom = function() {
-    $http.put('/api/user/poms/failed/' + self.today())
-      .success(function (status){
-      })
-      .error(function(status){
-        console.log('Total failure, try again.');
-      });
-  };
-
-  self.deletePom = function() {
-    return $http.delete('/api/user/poms/' + self.today())
-      .success(function (response){
-        return response;
-      })
-      .error(function(status){
-        console.log('Total failure, try again.');
-      });
-  };
-
   self.getPoms = function() {
-    return $http.get('/api/user/poms/' + self.today())
+    return $http.get('/api/user/poms/' + moment().format('YYYY-MM-DD'))
       .then(function(response) {
         return response.data;
       });
