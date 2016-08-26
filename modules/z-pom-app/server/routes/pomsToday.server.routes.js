392873d229650e5561d46be6feaec794335449a6
Jonathan McCormick <hello@jmccormick.me>
2016-08-26 11:32:59 -0600

    Rebuild pomsToday route to return poms from new schema
    Also renamed the file

--- modules/z-pom-app/server/routes/pomsToday.server.routes.js
+++ modules/z-pom-app/server/routes/pomsToday.server.routes.js
@@ -1,28 +1,23 @@
 'use strict';
 
 var mongoose = require('mongoose');
-var User = mongoose.model('User');
+var Pom = mongoose.model('Pom');
 
 module.exports = function(app) {
 
   app.route('/api/user/poms/:date')
   .get(function(req, res) {
 
-    User.aggregate([
-        { '$match': { '_id': req.user._id }},
-        { '$unwind': '$poms' },
-        { '$project': {
-            '_id': 0,
-            'poms.momentStarted': 1,
-            'poms.momentCompleted': 1
-        }},
-        { '$match': { 'poms.momentCompleted': { '$gte': new Date(req.params.date)} } }
+    Pom.aggregate([
+        { '$match': { 'userID': req.user._id }},
+        { '$match': { 'momentCompleted': { '$gte': new Date(req.params.date)} } }
       ],
       function(err,result) {
         if (err) {
           console.log(err);
           res.status(500);
         }
+        console.log(result);
         var pomsToday = result.length;
         res.json(pomsToday);
       }
