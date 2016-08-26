3cdc68ef188d72f1ecb916dcab038168b800ecda
Jonathan McCormick <hello@jmccormick.me>
2016-08-26 13:19:15 -0600

    Add new task route

---
+++ modules/z-pom-app/server/routes/new.task.server.routes.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var mongodb = require('mongodb');
+var mongoose = require('mongoose');
+var Task = mongoose.model('Task');
+
+module.exports = function(app) {
+
+  app.route('/api/user/new/task/:title')
+  .put(function(req, res) {
+
+    var task = new Task({
+        userID: req.user._id,
+        title: req.params.title
+    });
+
+    task.save(function(err, pom) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log(task);
+    });
+
+  });
+
+};
