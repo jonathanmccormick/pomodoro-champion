8e6cdc727d3b48ac484b5187058dd48ee1b7684f
Jonathan McCormick <hello@jmccormick.me>
2016-08-26 11:58:30 -0600

    Rebuild routes to return poms stored in new schema
    And cleaned up some debug/obsolete code.

--- modules/z-pom-app/server/routes/reports.server.routes.js
+++ modules/z-pom-app/server/routes/reports.server.routes.js
@@ -1,31 +1,20 @@
 'use strict';
 
 var mongoose = require('mongoose');
-var User = mongoose.model('User');
-// var moment = require('moment');
+var Pom = mongoose.model('Pom');
 
 module.exports = function(app) {
 
   app.route('/api/user/reports/:startDate/:endDate')
   .get(function(req, res) {
 
-    User.aggregate([
-        { '$match': { '_id': req.user._id }},
-        { '$unwind': '$poms' },
-        { '$project': {
-            '_id': 0,
-            'poms.momentStarted': 1,
-            'poms.momentCompleted': 1
-        }},
-        { '$match': { 'poms.momentStarted': { '$gte': new Date(req.params.startDate)} } },
+    Pom.aggregate([
+        { '$match': { 'userID': req.user._id }},
+        { '$match': { 'momentStarted': { '$gte': new Date(req.params.startDate)} } },
         { '$match': { '$or': [
-          { 'poms.momentCompleted': { '$lte': new Date(req.params.endDate)} } ,
-          { 'poms.momentCompleted': null } 
-        ] } },
-        { '$project': {
-            'momentStarted': '$poms.momentStarted',
-            'momentCompleted': '$poms.momentCompleted'
-        }}
+          { 'momentCompleted': { '$lte': new Date(req.params.endDate)} } ,
+          { 'momentCompleted': null } 
+        ] } }
 
       ],
       function(err,result) {
@@ -33,7 +22,6 @@ module.exports = function(app) {
           console.log(err);
           res.status(500);
         }
-        console.log(result);
         res.json(result);
       }
     );
