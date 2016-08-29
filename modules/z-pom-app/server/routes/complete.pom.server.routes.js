7bf041d15cdc85b86dac3b6168c0a2b66b77dc11
Jonathan McCormick <hello@jmccormick.me>
2016-08-29 13:13:52 -0600

    Increment selected task’s timeActual when pom completed

--- modules/z-pom-app/server/routes/complete.pom.server.routes.js
+++ modules/z-pom-app/server/routes/complete.pom.server.routes.js
@@ -4,12 +4,15 @@
 var mongoose = require('mongoose');
 // var User = mongoose.model('User');
 var Pom = mongoose.model('Pom');
+var Task = mongoose.model('Task');
 
 module.exports = function(app) {
 
   // Find pom with current ID, then set momentCompleted
   app.route('/api/user/pom/:id/complete/:moment')
   .put(function(req, res) {
+
+    var completedPom;
     
     Pom.findOneAndUpdate(
       { '_id': req.params.id, 'userID': req.user._id },
@@ -17,6 +20,18 @@ module.exports = function(app) {
       { save: true, new: true },
       function(err, doc) {
         if (err) return err;
+
+        completedPom = doc;
+
+        Task.findOneAndUpdate(
+        { '_id': completedPom.taskID, 'userID': req.user._id },
+        { $inc: { 'timeActual': 1 } },
+        function(err, doc) {
+          if (err) return res.sendStatus(err);
+          console.log(doc);
+          // res.sendStatus(200);
+        });
+
         console.log(doc);
         res.sendStatus(200);
       });
