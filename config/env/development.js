29c4a9d795f4812a9a76e56b9a81b4c184e4f982
Jonathan McCormick <hello@jmccormick.me>
2016-08-31 13:19:50 -0600

    Attempt to get db to work on production

--- config/env/development.js
+++ config/env/development.js
@@ -4,11 +4,10 @@ var defaultEnvConfig = require('./default');
 
 module.exports = {
   db: {
-    // uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/meanjs-dev',
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://ec2-54-199-245-156.ap-northeast-1.compute.amazonaws.com:27017/admin',
+    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/meanjs-dev',
     options: {
-      user: 'root',
-      pass: 'k2yNY5Teiewm'
+      user: '',
+      pass: ''
     },
     // Enable mongoose debug mode
     debug: process.env.MONGODB_DEBUG || false
